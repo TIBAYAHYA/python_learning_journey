ac8771652db781121dc0528c3c1cbb64799acbcd
TIBA YAHYA <ya7yatiba@gmail.com>
2024-11-11 20:59:10 +0100

    cool emails and others

--- emails/emailing_stuff.py
+++ emails/emailing_stuff.py
@@ -1,7 +1,16 @@
+""" simple code that send email to a destination email using smtplib module
+"""
 import smtplib
 conn = smtplib.SMTP('smtp.gmail.com', 587)
+email = input("enter your email: ")
+password = input("enter your password: ")
 #user_email = input("what is your email?:\n")
 #user_password = input("what is your password?:\n")
 conn.ehlo()
 conn.starttls()
-conn.login("yahyatiba12@gmail.com",)
+conn.login(email, password)
+destination = input("enter your destination email: ")
+subject = input("enter your subject: ")
+message = input("enter your message: ")
+conn.sendmail(email,destination,f"Subject: {subject} \n\n{message}")
+conn.quit()
