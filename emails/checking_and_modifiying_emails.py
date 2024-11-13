b9585cc1e2fe8ca59f4469e36177d0f1ecb6c97e
TIBA YAHYA <ya7yatiba@gmail.com>
2024-11-14 00:54:33 +0100

    tired upload

--- emails/checking_and_modifiying_emails.py
+++ emails/checking_and_modifiying_emails.py
@@ -23,9 +23,23 @@ message.text_part    #boolean for => Is the message a text?
 message.html_part.get_payload().decode("UTF-8")   #text of the email
 #example of deleting folders
 conn.select_folder("INBOX",readonly=False) #folder selection in read only mode being off
-UIDs = conn.search(["ON","11-Nov-2024"]) #list all messages received ON x date
-conn.delete_messages([UIDs]) #delete all emails that appear on the UIDs list, can also just give 1 email code aka something like nnnn (n is decimal number)
+UIDs = conn.search(["ALL"]) #list all messages received ON x date
+#conn.delete_messages([UIDs]) #delete all emails that appear on the UIDs list, can also just give 1 email code aka something like nnnn (n is decimal number)
+"""since alot of emails, and I mean ALOOOT OF EMAILS, are in html format, and I couldnt find one
+in txt format, I made a program that check my entire inbox and finds 1 singular gmail in txt format than exits out
+"""
+
+
+import sys
+rawer_message = conn.fetch([521],["BODY[]","FLAGS"])
+messaga = pyzmail.PyzMessage.factory(rawer_message[521][b"BODY[]"])
 
 
 
+print(messaga.text_part.get_payload().decode("UTF-8"))
+sys.exit()
+    
 conn.logout()
+"""finnaly found a text email!!! but Its past midnight and I missed some github daily upload points
+   plus I need to sleep
+"""
