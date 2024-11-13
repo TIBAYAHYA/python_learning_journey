6ecbc1c8581d5a40c1efce707c2fed5ac062cd7a
TIBA YAHYA <ya7yatiba@gmail.com>
2024-11-13 22:38:08 +0100

    more email stuff

---
+++ emails/very_easyGmail.py
@@ -0,0 +1,9 @@
+""" planned on using, ezgmail library, but then remembered that Im a coder and I love suffering, plus I dont think Its gonna be better, the thing is
+demanding an API while the other library only requires an app password, Im gonna stick to smtplib and imapclient
+"""
+import smtplib
+smtp_obj = smtplib.SMTP("smtp.gmail.com","587")
+smtp_obj.starttls()
+smtp_obj.login("my_email","password")
+smtp_obj.sendmail("my_email","target_email","Subject: hey bro.\nWhat is up, can you please do the stuff.Sincerement, him")
+smtp_obj.quit()
