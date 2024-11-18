0a2e414c01654d927d2bfe8559e176953619b69c
TIBA YAHYA <ya7yatiba@gmail.com>
2024-11-18 22:36:41 +0100

    unsub bot completed, with few bugs

--- emails and text messages/auto_unsubscriber.py
+++ emails and text messages/auto_unsubscriber.py
@@ -1,20 +1,35 @@
 """this program is supposed to iterate over my entire email list, find unsubscribe links, and automaticly opens them in browser
 
 """
+
 import imapclient
+import webbrowser
 conn = imapclient.IMAPClient("imap.gmail.com",ssl=True)
 email = input("Enter your Email: ")
 password = input("Enter Your Passsword: ")
 conn.login(email,password)
-conn.select_folder("INBOX",readonly=True)
+conn.select_folder("INBOX",readonly=True)  #INBOX folder, and read only to not risk deleting stuff
 messages_IDS = conn.search(["ALL"])  #a list of all emails IDS
 unsubscribe_links = []  #later to be used list
 
 import pyzmail,bs4
+
 for message_ID in messages_IDS:
-    raw_msg = conn.fetch([message_ID],["BODY[]","FLAGS"])   #
-    message = pyzmail.PyzMessage.factory(raw_msg[message_ID][b"BODY[]"])
-    if message.html_part:
-        html_content = message.html_part.get_payload().decode(message.html_part.charset)
+ #every single message id
+    print("Traiting message: %s"%(message_ID))
+    raw_msg = conn.fetch([message_ID],["BODY[]","FLAGS"])   #fetching the message
+    message = pyzmail.PyzMessage.factory(raw_msg[message_ID][b"BODY[]"])#parsing the message
+    if message.html_part: #checking If the message is html
+        html_content = message.html_part.get_payload().decode(message.html_part.charset if message.html_part.charset else "utf-8") #getting the html content
+        #bs4ing
         soup = bs4.BeautifulSoup(html_content,"html.parser")
-        
+        links = soup.find_all('a')
+        for link in links:
+            if 'unsubscribe' in link.get_text().lower() or 'unsubscribe' in link.get('href', '').lower(): #checking If the html  "a" has
+        #bs4ing        
+                if  not link.get('href').startswith('https://www.youtube.com'): #youtube links are not unsubscribe links 
+                    webbrowser.open(link.get('href'))  #opening the link in the browser
+
+
+
+
