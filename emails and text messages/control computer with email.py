1da3bd6437fa057eb83770207265342173b6124e
TIBA YAHYA <ya7yatiba@gmail.com>
2024-11-20 22:59:17 +0100

    email controls computer program, almost complete

--- emails and text messages/control computer with email.py
+++ emails and text messages/control computer with email.py
@@ -13,50 +13,62 @@ email = input("Enter your Email: ")
 password = input("Enter Your Passsword: ")
 conn.login(email,password)
 conn.select_folder("INBOX",readonly=False)  #INBOX folder
-start_now = datetime.datetime.now()
-formated_date = start_now.strftime("%d-%b-%Y")
-key_word = "Magito_the_idiot"
+start_now = datetime.datetime.now()  #the now time
+formated_date = start_now.strftime("%d-%b-%Y")   #correct format that gets accepted as a second argument of conn.search()
+key_word = "Magito_the_idiot"        #keyword to be checked in each mail
 
 
 
 
-initial_UIDS = conn.search(["SINCE",formated_date])
+
+initial_UIDS = conn.search(["SINCE",formated_date])  #searching for emails sent "today" according to time shot
 
 
-import sys###
 
 
+import subprocess
 
 import time,pyzmail
 def emails_checker():
-    deleting_list = []
-    conn.select_folder("INBOX",readonly=False)
+    deleting_list = [] # a list to store the emails that will be deleted later
+    conn.select_folder("INBOX",readonly=False) #updating the inbox folder
     
-    updated_uids = conn.search(["SINCE",formated_date])
+    updated_uids = conn.search(["SINCE",formated_date]) #checking email list again
     
-    updated_uids = [uid for uid in updated_uids if uid not in initial_UIDS]
+    updated_uids = [uid for uid in updated_uids if uid not in initial_UIDS] #selecting new emails
     
     
 
         
         
-    if not updated_uids:
+    if not updated_uids: #if no new emails
         print("No new emails")
         return
     for new_uid in updated_uids:
         raw_msg = conn.fetch([new_uid],["BODY[]","FLAGS"])   #fetching the message
         message = pyzmail.PyzMessage.factory(raw_msg[new_uid][b"BODY[]"])#parsing the message
-        if not message.text_part:
-            deleting_list.append(new_uid)
+        if not message.text_part: #if email has no txt part dont check It
             print("No text part found")
             continue
         email_content = message.text_part.get_payload().decode(message.text_part.charset)
-        if key_word not in email_content:
-            deleting_list.append(new_uid)
+        if key_word not in email_content: #if key word not found dont check it
             print("No key word found")
             continue
+        #when keyword is found, and email has a txt part, then the work starts!
+        split_email_content = email_content.split("\n")
+        #first element should be the keyword
+        #second element should be the command
+        #third element should be an optional a link or Idk
+        if split_email_content[1] == "torrent":
+            deleting_list.append(new_uid)
+            subprocess.Popen("C:\\Program Files (x86)\\qBittorrent\\qbittorrent.exe",split_email_content[2])
+            #todo, 
+            # add an actual downloable torrent file, to test the program
+            #delete the email after the command is executed
+            # add a confirmation email that signals that the progress worked or/ finished
+            #maybe add more commands
         
-        
+
                 
             
     
