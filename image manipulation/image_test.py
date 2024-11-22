ef7145cc4ea49f75fe4f0c2cf0e8b1265ba2a7a5
TIBA YAHYA <ya7yatiba@gmail.com>
2024-11-22 23:57:03 +0100

    more img manipulation testing

--- image manipulation/image_test.py
+++ image manipulation/image_test.py
@@ -2,6 +2,10 @@
 """
 from PIL import Image
 import os
+cwd = os.getcwd()
 os.chdir(r"C:\Users\maroc\OneDrive\Desktop\coding journey\automate_online-materials")
-cat_img = Image.open("zophie.png")
-print(cat_img)
+zophie_img = Image.open("zophie.png")
+print(zophie_img.size)
+cropped_zophie = zophie_img.crop((335, 345, 565, 560))
+os.chdir(cwd)
+cropped_zophie.save("cropped_zophie.png")
