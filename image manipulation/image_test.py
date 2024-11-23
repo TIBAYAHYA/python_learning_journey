7b9090bd8db36bd0d93f398c3472993a4ae51cc0
TIBA YAHYA <ya7yatiba@gmail.com>
2024-11-23 19:33:01 +0100

    image manipulator

--- image manipulation/image_test.py
+++ image manipulation/image_test.py
@@ -4,8 +4,14 @@ from PIL import Image
 import os
 cwd = os.getcwd()
 os.chdir(r"C:\Users\maroc\OneDrive\Desktop\coding journey\automate_online-materials")
-zophie_img = Image.open("zophie.png")
-print(zophie_img.size)
-cropped_zophie = zophie_img.crop((335, 345, 565, 560))
+zophie = Image.open("zophie.png")
 os.chdir(cwd)
-cropped_zophie.save("cropped_zophie.png")
+zophie_face = Image.open("zophie_face.png")
+zophie_width,zophie_height = zophie.size 
+zophie_face_width,zophie_face_height = zophie_face.size
+for top in range(0,zophie_height,zophie_face_height):
+    for left in range(0,zophie_width,zophie_face_width):
+        zophie.paste(zophie_face,(left,top))
+        print(left,top)
+zophie.save("cat_invasion.png")
+
