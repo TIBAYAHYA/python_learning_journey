353ab55ad40fd6f76c668b95f8325e8feea9e7c2
maroc <ya7yatiba@gmail.com>
2025-01-25 04:50:49 +0000

    more botting

--- Controlling the Keyboard and Mouse with GUI Automation/sushi go round game bot.py
+++ Controlling the Keyboard and Mouse with GUI Automation/sushi go round game bot.py
@@ -1,8 +1,11 @@
 """this program is supposed to play the web game <sushi go round><https://www.crazygames.fr/jeu/sushi-go-round> automaticly and efficiently for the user,
-
+25/01/2025 2:07 AM UPDATE: this project took longer than It should have, some dumb mistakes have been made by me, leading to an infinite tryal of fixing a non existent mistake, almost gave up on the whole thing,
+but thankfuly found out where the problem lays
 """
-import pyautogui,time,os
-os.chdir(r"C:\Users\maroc\OneDrive\Desktop\sushi go round images")
+import pyautogui,time,os,logging
+logging.basicConfig(level=logging.DEBUG)  
+
+#logging.disable(logging.debug)  #uncomment to disable debugs
 
 
 #a function that consistenly trys to open the target window
@@ -19,6 +22,7 @@ def actv_game():
         except IndexError:
             print("Window Not Found")
             time.sleep(1)
+    
 
 
 
@@ -39,19 +43,47 @@ def find_on_screen(image_fileName):
 
 
 
+#this function finds the coordinates of every ingredient
+def locate_ingredients_coordinates():  
+    global ingredients_locat
+    ingredients_locat = {}  #dictionaire of every ingredient and Its coordinates
+
+
+    for dirpath, dirnames, filenames in os.walk(r"C:\Users\maroc\OneDrive\Desktop\sushi go round images\ingredients"): #we walking over the ingredients images file
+            for filename in filenames:
+                    
+                ingredienta = filename[:-10]  # keep only the meaningful part of the file name
+                while True:
+                    try:
+                        ingredients_locat[ingredienta] = pyautogui.locateCenterOnScreen(os.path.join(dirpath,filename),confidence=0.9)  #we assign each ingredients an opposition coordinates in the form of a dictionary
+                        break
+                    except pyautogui.ImageNotFoundException: # try except loop to make sure It keeps trying until image matchs
+                        print("could not find: ",ingredienta," trying again...")
+                        time.sleep(1)
 
 
 
 
- 
-            
+
+
+
+
+
+
+
+
+
 actv_game()
 
+os.chdir(r"C:\Users\maroc\OneDrive\Desktop\sushi go round images")
 find_on_screen("Blue_PlayButton.png") #play button 
-
 find_on_screen("Yellow_SkipButton.png") #skip tutorial button
-
 find_on_screen("Purple_ContinueButton.png") #first stage button
+
+locate_ingredients_coordinates()
+logging.debug(ingredients_locat)
+
+
 
 
 
